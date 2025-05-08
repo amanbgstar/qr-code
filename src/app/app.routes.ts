54669227c33dcc7735d515305f56c7cc02d33309
amangaidhane <aman.b.gaidhane@gmail.com>
2025-05-08 09:41:56 +0530

    Modified routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
 
 export const routes: Routes = [
-    { path: 'qr', component: QrScannerComponent },
-    { path: '**', redirectTo: 'qr' }
+    { path: '', component: QrScannerComponent },
+    { path: '**', redirectTo: '' }
 ];
